914be37771b9fe14a0fe58717c6115e37fe0ebfa
AlexSuchot <alexandre.suchot@ynov.com>
2021-10-20 15:10:21 +0200

    feat: add cpu statistics content

--- Assets/Scripts/CPUStatistics.cs
+++ Assets/Scripts/CPUStatistics.cs
@@ -1,12 +1,68 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using Random = System.Random;
 
 public class CPUStatistics : MonoBehaviour
 {
-    public int Level;
+    public double Level = 0.5;
     private string Name;
-    public float MaxHealth;
-    public float CurrentHealth;
-    public float Attack;
-    public float Defense;
-    public float Speed;
+    public double MaxHealth;
+    public double CurrentHealth;
+    public double Attack;
+    public double Defense;
+    public double Speed;
+    
+    public void Start()
+    {
+        Random random = new Random();         
+        // Math.Round(random.NextDouble() * (this.Level - 0.01) + 0.01, 2); 
+        double HealthWeight = 0;
+        double AttackWeight = 0;
+        double DefenseWeight = 0;
+        double SpeedWeight = 0;
+
+        double[] n =
+        {
+            0,
+            Math.Round(random.NextDouble() * (this.Level - 0) + 0, 2),
+            Math.Round(random.NextDouble() * (this.Level - 0) + 0, 2),
+            Math.Round(random.NextDouble() * (this.Level - 0) + 0, 2),
+            this.Level
+        };
+        
+        Array.Sort(n);
+        
+        double[] stats =
+        {
+            HealthWeight, AttackWeight, DefenseWeight, SpeedWeight
+        };
+
+        for (int i = 1; i < n.Length; i++)
+        {
+            stats[i - 1] = n[i] - n[i - 1];
+            Debug.Log(stats[i - 1]);
+        }
+        // 0, X, Y, Z, 0.5
+    }
+
+    public double GetHealth()
+    {
+        return 0;
+    }
+
+    public double GetAttack()
+    {
+        return 0;
+    }
+    
+    public double GetDefense()
+    {
+        return 0;
+    }
+    
+    public double GetSpeed()
+    {
+        return 0;
+    }
 }
