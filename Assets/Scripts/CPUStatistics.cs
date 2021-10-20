c1a09e8092d7b4b9b154aaddb90e29cd0df66a66
AlexSuchot <alexandre.suchot@ynov.com>
2021-10-20 16:10:59 +0200

    feat: add functions for stats

--- Assets/Scripts/CPUStatistics.cs
+++ Assets/Scripts/CPUStatistics.cs
@@ -5,22 +5,17 @@ using Random = System.Random;
 
 public class CPUStatistics : MonoBehaviour
 {
-    public double Level = 0.5;
+    public double Level = 1;
     private string Name;
     public double MaxHealth;
     public double CurrentHealth;
     public double Attack;
     public double Defense;
     public double Speed;
-    
+
     public void Start()
     {
-        Random random = new Random();         
-        // Math.Round(random.NextDouble() * (this.Level - 0.01) + 0.01, 2); 
-        double HealthWeight = 0;
-        double AttackWeight = 0;
-        double DefenseWeight = 0;
-        double SpeedWeight = 0;
+        Random random = new Random();
 
         double[] n =
         {
@@ -30,39 +25,29 @@ public class CPUStatistics : MonoBehaviour
             Math.Round(random.NextDouble() * (this.Level - 0) + 0, 2),
             this.Level
         };
-        
+
         Array.Sort(n);
-        
-        double[] stats =
-        {
-            HealthWeight, AttackWeight, DefenseWeight, SpeedWeight
-        };
 
         for (int i = 1; i < n.Length; i++)
         {
-            stats[i - 1] = n[i] - n[i - 1];
-            Debug.Log(stats[i - 1]);
+            double j = 0.1 + n[i] - n[i - 1];
+            switch (i)
+            {
+                case 1:
+                    this.MaxHealth = 80 * j + 20;
+                    break;
+                case 2:
+                    this.Attack = 39 * j + 1;
+                    break;
+                case 3:
+                    this.Defense = 20 * j;
+                    break;
+                case 4:
+                    this.Speed = 6 * j + 6;
+                    break;
+            }
         }
-        // 0, X, Y, Z, 0.5
-    }
 
-    public double GetHealth()
-    {
-        return 0;
-    }
-
-    public double GetAttack()
-    {
-        return 0;
-    }
-    
-    public double GetDefense()
-    {
-        return 0;
-    }
-    
-    public double GetSpeed()
-    {
-        return 0;
+        this.CurrentHealth = this.MaxHealth;
     }
 }
